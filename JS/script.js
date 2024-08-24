100719cd4a6edcb525bd14fa53f6683f4f96a26e
Tobennaaustin <tobennaaustin@gmail.com>
2024-08-24 12:24:38 +0000

    First commit

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+function animator(){
+    window.location.href= "gallery.html";
+}
+function graphics(){
+    window.location.href= "web.html";
+}
+function backing(){
+    window.location.href= "web.html";
+}
+function hair(){
+    window.location.href= "web.html";
+}
+function crypto(){
+    window.location.href= "web.html";
+}
+function fineart(){
+    window.location.href= "web.html";
+}
+function fashion(){
+    window.location.href= "web.html";
+}
+function hair(){
+    window.location.href= "hair.html"
+}
